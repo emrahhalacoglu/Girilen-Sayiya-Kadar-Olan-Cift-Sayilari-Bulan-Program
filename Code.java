75be1fed2060a395962bb1d43df595daa4271968
ehm <emrahhalacoglu@gmail.com>
2022-11-01 08:58:37 +0300

    Code file hase been added.

---
+++ Code.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Bir sayi giriniz : ");
+        int sayi=input.nextInt();
+        int i;
+        int c=-1;
+        int sum=0;
+
+        for(i=0; i<=sayi; i++){
+            if(i%3==0&&i%4==0){sum+=i;c++;}
+        }
+        System.out.println("3 ve 4 e bolunen sayilarin ortalamasi : "+sum/c);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Girilen-Sayiya-Kadar-Olan-Cift-Sayilari-Bulan-Program
-Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan program.
+# Girilen-Sayiya-Kadar-3-Ve-4-Katlari-Ortalamasi
+Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+
+[Patika.dev](https://www.patika.dev/tr)
